93f9b3389c422ff6ea7b5b2111f1e87f05d7693d
mangwoncassie <dudnfsla1@naver.com>
2023-11-12 18:01:09 +0900

    update is done

---
+++ index.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const userRouter = require("./routes/user");
+const authRouter = require("./routes/auth");
+
+
+dotenv.config();
+
+mongoose.connect(
+    process.env.MONGO_URL
+    )
+    .then(() => console.log("DBConnection Success"))
+    .catch((err => {
+            console.log(err);
+        }));
+
+
+app.use(express.json());
+app.use("/api/auth", authRouter);
+app.use("/api/users", userRouter);
+
+app.listen(process.env.PORT || 5000, () => {
+    console.log("backend server is running");
+})
+
+
+
+
+
+
