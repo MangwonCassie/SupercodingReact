cbef3bff8970a5121b85566108553c26fec69eee
CASSIE1107 <dudnfsla1@naver.com>
2023-12-27 12:29:42 +0900

    꼬인거 해결 중

---
+++ api/routes/verifyToken.js
@@ -0,0 +1,63 @@
+const jwt = require("jsonwebtoken");
+
+const generateTokens = (user) => {
+  const accessToken = jwt.sign(
+    {
+      id: user._id,
+      isAdmin: user.isAdmin,
+    },
+    process.env.JWT_SECRET,
+    { expiresIn: "1y" } // 액세스 토큰 만료 시간
+  );
+
+  const refreshToken = jwt.sign(
+    {
+      id: user._id,
+      isAdmin: user.isAdmin,
+    },
+    process.env.REFRESH_TOKEN_SECRET,
+    { expiresIn: "1y" } // 리프레시 토큰 만료 시간
+  );
+
+  return { accessToken, refreshToken };
+};
+
+const verifyToken = (req, res, next) => {
+  const authHeader = req.headers.token;
+  if (authHeader) {
+    const token = authHeader.split(" ")[1];
+    jwt.verify(token, process.env.JWT_SEC, (err, user) => {
+      if (err) res.status(403).json("Token is not valid!");
+      req.user = user;
+      next();
+    });
+  } else {
+    return res.status(401).json("You are not authenticated!");
+  }
+};
+
+const verifyTokenAndAuthorization = (req, res, next) => {
+  verifyToken(req, res, () => {
+    if (req.user.id === req.params.id || req.user.isAdmin) {
+      next();
+    } else {
+      res.status(403).json("You are not alowed to do that!");
+    }
+  });
+};
+
+const verifyTokenAndAdmin = (req, res, next) => {
+  verifyToken(req, res, () => {
+    if (req.user.isAdmin) {
+      next();
+    } else {
+      res.status(403).json("You are not alowed to do that!");
+    }
+  });
+};
+
+module.exports = {
+  verifyToken,
+  verifyTokenAndAuthorization,
+  verifyTokenAndAdmin,
+};
