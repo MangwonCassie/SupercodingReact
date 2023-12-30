ffa2e0f1981572838a69c73f1cd923f569ce52e6
CASSIE1107 <dudnfsla1@naver.com>
2023-12-30 19:12:17 +0900

    feat: redux 설치 후 버전 충돌 react-router-dom@6.0.0로 업그레이드 해야 동작함

---
+++ client/src/redux/cartRedux.js
@@ -0,0 +1,28 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+
+//first object 생성
+//payload = new product
+//toolkit에서만 이렇게 가능
+const cartSlice = createSlice({
+    name: "cart",
+    initialState:{
+        products: [],
+        quantity: 0,
+        total: 0,
+    },
+    reducers: {
+        addProduct: (state, action) => {
+            state.quantity += 1;
+            state.products.push(action.payload.product)
+            state.total += action.payload.price;
+        },
+    },
+});
+
+
+export const {addProduct} = cartSlice.actions;
+export default cartSlice.reducer;
+
+
+
---
+++ client/src/redux/store.js
@@ -0,0 +1,12 @@
+import { configureStore } from "@reduxjs/toolkit";
+import cartReducer from "./cartRedux";
+
+
+//모든 리듀서 다 기재해야함
+//주의해야 할 부분은 reducer가 아니라 reducers가 아닌 reducer 필드
+
+export default configureStore({
+    reducer: {
+        cart: cartReducer,
+    },
+});
