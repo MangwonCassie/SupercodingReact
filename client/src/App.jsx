f591a0e4d149944c0f426bcc08e5d8b0063ced29
CASSIE1107 <dudnfsla1@naver.com>
2024-01-07 16:03:27 +0900

    feat: 리덕스 스토어에서 필요한 상태 가져오는 useSelector 이용해서 로그인된 사용자 정보 가져오도록 수정함

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -11,9 +11,10 @@ import {
   Navigate,
   
 } from "react-router-dom";
+import { useSelector } from "react-redux";
 
 const App = () => {
-  const user = false
+  const user = useSelector((state)=>state.user.currentUser);
   //이거 user=false로 해야 login됨
   return (<Router>
     <Routes>
