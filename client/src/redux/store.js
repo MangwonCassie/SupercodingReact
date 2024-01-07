077986752b70c442ee511b22d3f47a981439c44c
CASSIE1107 <dudnfsla1@naver.com>
2024-01-07 20:10:42 +0900

    feat: feat: 지금 상태에는 로그인한 뒤에 login페이지가면 또 가짐 (user가 없어서 initial state로 가버려서) persist state를 해줘야함. Redux-persist 상위 프로젝트에서 설치 후 사용, index.js에서도 PersistGate App 감싸줘야함

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -2,13 +2,48 @@ import { configureStore } from "@reduxjs/toolkit";
 import cartReducer from "./cartRedux";
 import userReducer from "./userRedux";
 
+//로그인 후 유저갖고 있게끔 처리
+import {
+    persistStore,
+    persistReducer,
+    FLUSH,
+    REHYDRATE,
+    PAUSE,
+    PERSIST,
+    PURGE,
+    REGISTER,
+  } from 'redux-persist'
+  import storage from 'redux-persist/lib/storage'
+
+
+  const persistConfig = {
+    key: 'root',
+    version: 1,
+    storage,
+  }
+  
+
+  //홈페이지 rootReducer를 userReducer로
+  const persistedReducer = persistReducer(persistConfig, userReducer);
+
 
 //모든 리듀서 다 기재해야함
 //주의해야 할 부분은 reducer가 아니라 reducers가 아닌 reducer 필드
 
-export default configureStore({
+export const store = configureStore({
     reducer: {
         cart: cartReducer,
-        user: userReducer,
+        user: persistedReducer,
     },
+
+    //추후에 공부하면됨
+    middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
 });
+
+//index.js에 연결
+export let persistor = persistStore(store);
