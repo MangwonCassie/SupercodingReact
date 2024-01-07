9f1762934f75f14de30aa62363a381e8e5e61318
CASSIE1107 <dudnfsla1@naver.com>
2024-01-07 15:43:24 +0900

    feat: styled-components 문법 &:disabled 사용하여 버튼 컴포넌트가 비활성화일 때 스타일 적용

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -51,6 +51,11 @@ const Button = styled.button`
   color: white;
   cursor: pointer;
   margin-bottom: 10px;
+
+  &:disabled{
+    color:green;
+    cursor: not-allowed;
+  }
 `;
 
 const Link = styled.a`
