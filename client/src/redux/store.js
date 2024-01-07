81ddb2a81ce6c09ba9569736194d2832f5d3e47a
CASSIE1107 <dudnfsla1@naver.com>
2024-01-07 20:35:29 +0900

    feat: cart를 구매하고싶으면 combinedReducers import 후, persistConfig도 root 이름 바꾸고 cart, user 합친다. 그 후 persistReducer안에 rootReducer로 넣어줌 그러면 reducer 속성에 내용 없앨 수 있음, 그렇게 하면 cart에 상품 올리고 홈페이지 메인가도 그대로 있음

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,4 +1,4 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
 import cartReducer from "./cartRedux";
 import userReducer from "./userRedux";
 
@@ -22,19 +22,16 @@ import {
     storage,
   }
 
+  const rootReducer = combineReducers({user: userReducer, cart: cartReducer});
+
   //홈페이지 rootReducer를 userReducer로
-  const persistedReducer = persistReducer(persistConfig, userReducer);
+  const persistedReducer = persistReducer(persistConfig, rootReducer);
 
-
-//모든 리듀서 다 기재해야함
+  
 //주의해야 할 부분은 reducer가 아니라 reducers가 아닌 reducer 필드
 
 export const store = configureStore({
-    reducer: {
-        cart: cartReducer,
-        user: persistedReducer,
-    },
-
+    reducer: persistedReducer,
     //추후 팔로우업
     middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware({
