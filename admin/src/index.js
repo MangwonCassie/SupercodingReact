dbff4595adad83b221c2c41522dcbe9908a85bcb
CASSIE1107 <dudnfsla1@naver.com>
2024-01-15 12:13:19 +0900

    feat: 백엔드 heroku deploy

--- admin/src/index.js
+++ admin/src/index.js
@@ -13,3 +13,12 @@ ReactDOM.render(
   </Provider>,
   document.getElementById("root")
 );
+
+//NOTE: heroku 설정
+if(process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "../client", "build", "index.html"))
+  })
+}
