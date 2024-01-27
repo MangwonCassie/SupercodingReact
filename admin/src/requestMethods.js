ff8c707b30a2b0881be5ba510f126d1e702672ef
MangwonCassie <dudnfsla1@naver.com>
2024-01-27 19:55:04 +0900

    error: admin페이지 accessToken 문제 발견 아무것도 렌더링 안됨

--- admin/src/requestMethods.js
+++ admin/src/requestMethods.js
@@ -3,6 +3,8 @@ import axios from "axios";
 const BASE_URL = "http://localhost:5000/api/"
 const TOKEN =   JSON.parse(JSON.parse(localStorage.getItem("persist:root")).user).currentUser.accessToken;
 
+console.log("admin 디버깅 테스트, TOKEN", TOKEN);
+
 export const publicRequest = axios.create({
     baseURL: BASE_URL,
 });
